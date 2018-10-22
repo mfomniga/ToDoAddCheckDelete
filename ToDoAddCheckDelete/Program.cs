c0ff83baa2e62f239a0b6641659cedd5b4ca1115
mfomniga <44165437+mfomniga@users.noreply.github.com>
2018-10-22 16:39:51 +0200

    Initial commit

---
+++ ToDoAddCheckDelete/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ToDoAddCheckDelete
+{
+    class Program
+    {
+        private static List<string> namen = new List<string>();
+
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Benutze die Befehle Add, Check und Delete!");
+            
+            // add mathias
+            string userInput = Console.ReadLine();
+
+            if(userInput.StartsWith("add "))
+            {
+                Console.WriteLine("Gib den Namen der Person ein die zu hinzufügen willst:");
+                //string userInputName = Console.ReadLine();
+                namen.Add(Console.ReadLine());
+
+                Console.WriteLine(namen.Find();
+            }
+            else if(userInput.StartsWith("delete "))
+            {
+
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
